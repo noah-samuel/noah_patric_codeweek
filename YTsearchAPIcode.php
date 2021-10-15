872a26b753df1239f8e86e06290f31ced5d6e88d
noah-samuel <91123321+noah-samuel@users.noreply.github.com>
2021-10-15 16:25:56 +0200

    final version

--- YTsearchAPIcode.php
+++ YTsearchAPIcode.php
@@ -1,40 +1,73 @@
 <?php
-
-echo "\n\n\n";
-        printf("Suchbegriff (Zum Beispiel: Championsleague, SRF, GTA, Fifa; (Bei Suchanfrage mit mehreren Wörtern, zwischen jedem wort ein '+' setzen. Z.B. Cristiano+Ronaldo+Freistoss+Tor) \n\n");
-        $title = readline(); //Zum Beispiel: Championsleague, SRF, GTA, Fifa; (Bei Suchbegriff mit mehreren Wörtern, zwischen jedem wort ein '+' setzen. Z.B. Cristiano+Ronaldo+Freistoss+Tor)
-        echo "\n";
-        printf("Anzahl Ergebnisse (Zahl) \n\n");
-        $maxResults = readline();  //Eine beliebige Nummer eingeben. Z.B. 5;
-        echo "\n";
-        printf("Sortierung angezeigter Ergebnisse: (date, viewCount, relevance, title) \n\n");
-        $order = readline(); //Zum Beispiel: date, viewCount, relevance, title einsetzbar. Sortiert erhaltene Informationen nach diesen Eigenschaften;
-        echo "\n";
-        printf("Videolänge: (any) \n\n");
-        $videoduration = readline();  //Zum Beispiel: any einsetzbar;
-        $key = "AIzaSyDNvoF8RPPyjTatjiOxiLb1HFMSTG4IL6Y";
-
-
-
-$curl_session = curl_init();
-
-curl_setopt($curl_session, CURLOPT_RETURNTRANSFER, true);
-//step2
-curl_setopt($curl_session, CURLOPT_URL, "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=$maxResults&format=json&q=$title&order=$order&videoDuration=$videoduration&key=$key");
-//step3
-$result = json_decode(curl_exec($curl_session), true);
-//step4
-curl_close($curl_session);
-//step5
-//print_r($result);
-echo "\n\n";
-foreach ($result["items"] as $item) {
-    print "\n\n\n\n\n\n";
-    print ("Title: " . $item ["snippet"] ["title"] . "\n\n");
-    print ("Channel: " . $item ["snippet"] ["channelTitle"] . "\n\n");
-    print ("Published at: " . $item ["snippet"] ["publishedAt"] . "\n");
-    print ("kind: " . $item ["id"] ["kind"] . "\n\n");
-    print ("Video ID: " . $item ["id"] ["videoId"] . "\n");
-    print ("Channel ID: " . $item ["snippet"] ["channelId"] . "\n\n");
-    print ("Thumbnail URL: " . $item ["snippet"] ["thumbnails"] ["high"] ["url"] . "\n\n");
-}
+    //code
+?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+​
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+    <title> Projekt Codeweek</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" />
+    <meta name="viewport" content="width=device-width, initial-scale=no">
+</head>
+​
+<body>
+<div class="wrapper">
+    <img src="logo.png" alt="" height="100px">
+    <div class="box">
+        <input type="submit" id="check">
+            <div class="search-box">
+                <form method="GET">
+                    <input type="text" name="q" placeholder="Type here...">
+                    <label for="check" class="icon">
+                    <i class="fas fa-search"></i> 
+                    <?php
+                        if(!empty($_GET["q"])){
+                            $key = "AIzaSyAF98o-1B4vAX8wUPswd12L6oncMQbAiRc";
+                            $curl_session = curl_init();
+                            curl_setopt($curl_session, CURLOPT_RETURNTRANSFER, true);
+                            $q = $_GET["q"];
+                            $q = urlencode($q);
+                            $url = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=10000&format=json&q=$q&order=relevance&videoDuration=any&key=$key";
+                            curl_setopt($curl_session, CURLOPT_URL, $url);
+                            $result = json_decode(curl_exec($curl_session), true);
+                            curl_close($curl_session);
+                        }
+                    ?>
+                </form>
+            </label>
+        </div>
+    </div>
+</div>
+​
+<div class="flexbox-container">
+    <?php
+        foreach ($result["items"] as $item) {
+            $title = $item["snippet"]["title"];
+            $channelTitle = $item["snippet"]["channelTitle"];
+            $date = $item["snippet"]["publishedAt"];
+            $thumbnail = $item["snippet"]["thumbnails"]["medium"]["url"];
+            if( empty($item["id"]["videoId"]) ){
+                $videolink = "https://www.youtube.com/channel/" . $item["id"]["channelId"];
+            }else{
+                $videolink = "https://youtube.com/watch?v=" . $item["id"]["videoId"];
+            }
+            echo "
+            <a href='$videolink'><div class='flexbox-item flexbox-item'>
+                <img src='$thumbnail' alt='krank'>
+                <div class='text'>
+                    <h1>$title</h1>
+                    <p>$channelTitle</p>
+                    <p>$date</p>
+                </div>
+            </div></a>";
+        }
+    ?>
+​
+</div>
+​
+<!-- ----------------------------------------------------------------------------------------------- -->
+</body>
+​
+</html>
