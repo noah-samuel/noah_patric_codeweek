3f145efc19531dbed5d674cf9c23727a7df90ceb
patrickcilmi <91224263+patrickcilmi@users.noreply.github.com>
2021-10-15 09:41:49 +0200

    Update YTsearchAPIcode.php
    Suchanfrage nach bestimmten Informationen nun filterbar.

--- YTsearchAPIcode.php
+++ YTsearchAPIcode.php
@@ -8,43 +8,33 @@
         $order = readline(); //Zum Beispiel: date, videoCount, viewCount, relevance, title einsetzbar. Sortiert erhaltene Informationen nach diesen Eigenschaften;
         printf("Videolänge: any, short, medium, long \n");
         $videoduration = readline();  //Zum Beispiel: any, short, medium, long einsetzbar;
-        printf("Dein API key: \n");
-        $key = readline(); //Dein API-Key;
+        $key = "AIzaSyDNvoF8RPPyjTatjiOxiLb1HFMSTG4IL6Y";
+
+
+
+
 
 
 $curl_session = curl_init();
+
+curl_setopt($curl_session, CURLOPT_RETURNTRANSFER, true);
 //step2
 curl_setopt($curl_session, CURLOPT_URL, "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=$maxResults&format=json&q=$title&order=$order&videoDuration=$videoduration&key=$key");
 //step3
-$result = curl_exec($curl_session);
+$result = json_decode(curl_exec($curl_session), true);
 //step4
 curl_close($curl_session);
 //step5
-echo $result;
-
-
-// Abhängig von der API, hier json
-$headers = array(
-    'Accept: application/json',
-    'Content-Type: application/json',
-);
-
-$curl = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=$maxResults&format=json&q=$title&order=$order&videoDuration=$videoduration&key=$key";
-$url = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=$maxResults&format=json&q=$title&order=$order&videoDuration=$videoduration&key=$key";
-
-$curl = curl_init();
-curl_setopt($curl, CURLOPT_URL, $url);
-curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-$response = curl_exec($curl);
-$code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-curl_close($curl);
-if ($code == 200) {
-    $response = json_decode($response, true);
-    print_r($response);
-} else {
-    echo 'error ' . $code;
+//print_r($result);
+echo "\n\n";
+foreach ($result["items"] as $item) {
+    print "\n\n\n\n\n";
+print ($item ["snippet"] ["title"] . "\n\n");
+print ($item ["snippet"] ["channelTitle"] . "\n\n");
+print ($item ["snippet"] ["publishedAt"] . "\n");
+print ($item ["id"] ["kind"] . "\n\n");
+print ($item ["id"] ["videoId"] . "\n");
+print ($item ["snippet"] ["channelId"] . "\n\n");
+print ($item ["snippet"] ["thumbnails"] ["high"] ["url"]);
 }
 
